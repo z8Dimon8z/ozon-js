0780e782df10fa9a290004fb9f62f2b602cb1316
z8Dimon8z <dima-lysak@yandex.ru>
2022-11-12 20:06:09 +0300

    второй день интенсива

---
+++ src/modules/getData.js
@@ -0,0 +1,9 @@
+const getData = () => {
+  return fetch('https://new-glo-ozon-baze-default-rtdb.firebaseio.com/goods.json')
+  .then((response) => {
+    return response.json();
+  });
+  
+};
+
+export default getData;
--- src/modules/second.js
+++ src/modules/second.js
@@ -1,5 +1,26 @@
+import getData from "./getData.js";
+import postData from "./postData.js";
+
 const second = () => {
-  console.log('файл second');
+
+  const cartBtn = document.getElementById('cart');
+
+
+  cartBtn.addEventListener('click', () => {
+    //   postData().then((data) => {
+    //     console.log(data);
+
+    //   getData().then((data) => {
+    //       console.log(data);
+    //     });
+    //   });
+    // });
+    getData().then((data) => {
+      console.log(data);
+    });
+
+  });
+
 };
 
 export default second;
